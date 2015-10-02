b5f59daaf1d1b2b6b553e3c87ac4335d14dbdd87
quangdung92 <quangdung.hvbc@gmail.com>
2015-10-02 09:10:09 +0700

    new-demo

---
+++ demo3/app/src/main/java/com/example/quangdung/demo3/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.quangdung.demo3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubAct1.class);
+                startActivity(intent);
+            }
+        });
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubAct2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText
+                (this,"on remuse", Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(
+          this,"on Pause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(
+                this, "on Stop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected  void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "on Destroy", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
