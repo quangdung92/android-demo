d15c57999b799eb1de4d24f0fb6bc2d76a77b858
dungGrooovy <mayonesama12@gmail.com>
2016-05-31 10:55:10 +0700

    new update

--- demo3/app/src/main/gen/com/example/quangdung/demo3/BuildConfig.java
+++ demo3/app/src/main/gen/com/example/quangdung/demo3/BuildConfig.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package android.support.v7.appcompat;
+package com.example.quangdung.demo3;
 
 /* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
 public final class BuildConfig {
